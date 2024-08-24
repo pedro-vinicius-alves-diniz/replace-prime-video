74519fdcde9173543256f7a6b0bb5528e78cdb39
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-08-24 22:42:04 +0200

    update header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'azul-claro': '#0F79AF',
+        'azul-escuro': '#00050D',
+        'cinza-escuro': '#191E25'
+      }
+    },
+  },
+  plugins: [],
+}
+
