2588168ad767567ec969b118e0af4f94174461da
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-08-25 18:14:30 +0200

    finalizado seção UEFA

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         'azul-claro': '#0F79AF',
         'azul-escuro': '#00050D',
         'cinza-escuro': '#191E25'
+      },
+
+      backgroundImage: {
+        'home': 'url("/src/assets/img/bg.jpg")',
+        'uefa': 'url("/src/assets/img/bg-uefa.jpg")'
+      },
+      
+      fontFamily: {
+        'poppins': ['Poppins', 'san-serif']
       }
     },
   },
